67921a52fc5376f41377fa60f3d3e49b7a69a90d
Fernando Lopez <37802572+Cainuriel@users.noreply.github.com>
2023-09-05 00:14:07 +0200

    primeras pruebas

--- plantillas/randomNumber.js
+++ plantillas/randomNumber.js
@@ -1,3 +1,19 @@
+let clicks = 0
+
 function generateRandomNumber() {
   return Math.floor(Math.random() * 11);
+}
+
+document.getElementById('counter').textContent = clicks;
+
+function clickeando() {
+  clicks++;
+  document.getElementById('counter').textContent = clicks;
+
+}
+
+function comprobar() {
+  let match = generateRandomNumber();
+  alert(`Número de clicks: ${clicks}. Número aleatorio: ${match}`);
+  location.reload();
 }
